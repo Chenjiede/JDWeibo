46b0f7c35f474e1e5f4f0192ada43af946fd9108
chen <409303584@qq.com>
2017-03-16 22:19:49 +0800

    测试提交

--- JDWeibo/JDWeibo/Classes/Tool/NetworTools.swift
+++ JDWeibo/JDWeibo/Classes/Tool/NetworTools.swift
@@ -7,18 +7,18 @@
 //
 
 import Foundation
-import AFNetworking
+//import AFNetworking
 
-class NetworTools: AFHTTPSessionManager {
-    
-    static let shareInstance : NetworTools = {
-        let url = URL(string: "https://api.weibo.com/")
-
-        let instance = NetworTools(baseURL: url, sessionConfiguration: .default)
-        instance.responseSerializer.acceptableContentTypes = NSSet(objects: "application/json", "text/json", "text/javascript", "text/plain") as? Set
-        
-        return instance
-    }()
-    
-}
+//class NetworTools: AFHTTPSessionManager {
+//    
+//    static let shareInstance : NetworTools = {
+//        let url = URL(string: "https://api.weibo.com/")
+//
+//        let instance = NetworTools(baseURL: url, sessionConfiguration: .default)
+//        instance.responseSerializer.acceptableContentTypes = NSSet(objects: "application/json", "text/json", "text/javascript", "text/plain") as? Set
+//        
+//        return instance
+//    }()
+//    
+//}
 
